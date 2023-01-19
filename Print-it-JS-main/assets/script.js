968a11389b188a6bcaaeb4218705678d0b9682e7
Eddy <eddytougon@gmail.com>
2023-01-19 22:15:45 +0100

    fear: syteme carousel avec fleche

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -16,4 +16,33 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+const Fleche_droite = document.querySelector('.arrow_right');
+const Fleche_gauche = document.querySelector('.arrow_left');
 
+const slide = document.querySelector('.slide');
+const tagLine = document.querySelector('.tagline');
+
+Fleche_droite.addEventListener('click', Slide_suivant);
+Fleche_gauche.addEventListener('click', Slide_precedent);
+let Image_actuel=0
+
+function Slide_suivant() {
+	Image_actuel++;
+	console.log(Image_actuel)
+	if (Image_actuel >  slides.length - 1) {
+		Image_actuel = 0;
+	}
+	slide.src = './assets/images/slideshow/' + slides[Image_actuel].image;
+	tagLine.innerHTML = slides[Image_actuel].tagLine;
+}
+
+function Slide_precedent() {
+
+	Image_actuel--;
+	if (Image_actuel < 0) {
+		Image_actuel = slides.length - 1;
+	}
+
+	slide.src = './assets/images/slideshow/' + slides[Image_actuel].image;
+	tagLine.innerHTML = slides[Image_actuel].tagLine;
+}
