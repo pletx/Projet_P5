12dca33b8dd3180306e0ed9ade4a02833b929339
Eddy <eddytougon@gmail.com>
2023-02-08 06:34:28 +0100

    feat:ajout pointer +jsdoc

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -23,11 +23,16 @@ const slide = document.querySelector('.slide');
 const tagLine = document.querySelector('.tagline');
 
 const dots = document.querySelectorAll('.dot');
+let Image_actuel=0
 
 Fleche_droite.addEventListener('click', Slide_suivant);
 Fleche_gauche.addEventListener('click', Slide_precedent);
-let Image_actuel=0
 
+/**
+augmente la valeur de la variable
+   "Image_actuel" de 1, 
+  la prochaine image sera affichée lors de l'exécution de la fonction.
+ */
 function Slide_suivant() {
 	Image_actuel++;
 	console.log(Image_actuel)
@@ -38,24 +43,38 @@ function Slide_suivant() {
 	tagLine.innerHTML = slides[Image_actuel].tagLine;
 	Changment_point()
 }
-
+/**
+diminue la valeur de la variable
+   "Image_actuel" de 1, 
+  l'image précedent sera affichée lors de l'exécution de la fonction.
+ */
 function Slide_precedent() {
-
 	Image_actuel--;
 	if (Image_actuel < 0) {
 		Image_actuel = slides.length - 1;
 	}
 
 	slide.src = './assets/images/slideshow/' + slides[Image_actuel].image;
-
 	tagLine.innerHTML = slides[Image_actuel].tagLine;
-
-
 	Changment_point() 
 }
+
+
+
+
+/**
+Parcour chaque élément dans le tableau "dots".
+
+Supprime la classe "dot_selected"pour
+ chaque point donc aucun point n'est actuellement sélectionné.
+
+Ajoute la classe "dot_selected" au point au rang correspondant
+ à la valeur actuelle de la variable "Image_actuel".
+ */
 function Changment_point() {
     dots.forEach(function(dot) {
         dot.classList.remove('dot_selected');
     });
     dots[Image_actuel].classList.add('dot_selected');
 }
+
